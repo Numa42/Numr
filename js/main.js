7103e24efd1ddce626ccbb4baa5e3368d60fbe75
Numa42 <mizuki42m@gmail.com>
2024-04-16 23:22:25 +0200

    Animation titre

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ const lenis = new Lenis()
 const pageTitle = document.getElementById('title');
 
 // Fonction à exécuter lors du scroll
-lenis.on('scroll', () => {
-    // Récupération de la position de défilement verticale
-    const scrollY = lenis.scroll.y;
-
+lenis.on('scroll', (e) => {
     // Calcul de la nouvelle taille de texte en fonction de la position de défilement
     // Vous pouvez ajuster ces valeurs selon vos besoins
-    const newSize = 31.3 - scrollY * 0.05; // Rétrécissement progressif du titre
+    const newSize = 30 - scrollY * 0.03; // Rétrécissement progressif du titre
+    console.log(newSize);
 
     // Application de la nouvelle taille de texte au titre
     pageTitle.style.fontSize = newSize + 'vw';
+    pageTitle.style.letterSpacing = newSize / -10.7143 + 'vw';
+    pageTitle.style.marginBottom = newSize - 29 + 'vw';
+    pageTitle.style.marginTop = newSize / 30 + 'vw';
 });
 
 function raf(time) {
